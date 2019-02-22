b13a30f0bd46cdad816833fca8d3599d5bad9fc2
JackSilence <jacksilence@gmail.com>
2019-02-22 16:17:11 +0800

    改用gmail

--- src/main/java/shopee/App.java
+++ src/main/java/shopee/App.java
@@ -1,13 +1,47 @@
 package shopee;
 
+import java.util.Properties;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class App {
+	@Value( "${email.username}" )
+	private String username;
+
+	@Value( "${email.password}" )
+	private String password;
+
 	public static void main( String[] args ) {
 		SpringApplication.run( App.class, args );
+	}
+
+	@Bean
+	public JavaMailSender sender() {
+		JavaMailSenderImpl sender = new JavaMailSenderImpl();
+
+		sender.setHost( "smtp.gmail.com" );
+		sender.setPort( 465 );
+
+		sender.setUsername( new String( Base64.decodeBase64( username ) ) );
+		sender.setPassword( new String( Base64.decodeBase64( password ) ) );
+
+		Properties props = sender.getJavaMailProperties();
+
+		props.put( "mail.transport.protocol", "smtp" );
+		props.put( "mail.smtp.auth", "true" );
+		props.put( "mail.smtp.starttls.enable", "true" );
+		props.put( "mail.debug", "true" );
+		props.put( "mail.smtp.ssl.enable", "true" );
+
+		return sender;
 	}
 }
--- src/main/java/shopee/service/MailService.java
+++ src/main/java/shopee/service/MailService.java
@@ -1,47 +1,23 @@
 package shopee.service;
 
-import java.io.IOException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import com.sendgrid.Content;
-import com.sendgrid.Email;
-import com.sendgrid.Mail;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
 
 @Service
 public class MailService implements IMailService {
-	private final Logger log = LoggerFactory.getLogger( this.getClass() );
-
-	@Value( "${sendgrid.api-key}" )
-	private String key;
+	@Autowired
+	private JavaMailSender sender;
 
 	@Override
 	public void send( String subject, String content ) {
-		Email from = new Email( "shopee@heroku.com" ), to = new Email( "jacksilence@gmail.com" );
+		SimpleMailMessage message = new SimpleMailMessage();
 
-		Mail mail = new Mail( from, subject, to, new Content( "text/html", content ) );
+		message.setTo( "lethington@gmail.com" );
+		message.setSubject( subject );
+		message.setText( content );
 
-		Request request = new Request();
-
-		try {
-			request.setMethod( Method.POST );
-			request.setEndpoint( "mail/send" );
-			request.setBody( mail.build() );
-
-			Response response = new SendGrid( key ).api( request );
-
-			log.info( "Status: {}", response.getStatusCode() );
-
-		} catch ( IOException e ) {
-			throw new RuntimeException( e );
-
-		}
+		sender.send( message );
 	}
 }
