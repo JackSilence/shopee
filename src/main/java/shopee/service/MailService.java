0c43ddfe2a23583cdaf9f0ce355311a89af382d5
JackSilence <jacksilence@gmail.com>
2019-02-22 16:27:20 +0800

    改用MimeMessage

--- src/main/java/shopee/service/MailService.java
+++ src/main/java/shopee/service/MailService.java
@@ -1,23 +1,43 @@
 package shopee.service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.lang3.CharEncoding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailService implements IMailService {
+	private final Logger log = LoggerFactory.getLogger( this.getClass() );
+
 	@Autowired
 	private JavaMailSender sender;
 
 	@Override
 	public void send( String subject, String content ) {
-		SimpleMailMessage message = new SimpleMailMessage();
+		try {
+			log.info( subject + "發送開始!" );
 
-		message.setTo( "lethington@gmail.com" );
-		message.setSubject( subject );
-		message.setText( content );
+			MimeMessage message = sender.createMimeMessage();
 
-		sender.send( message );
+			MimeMessageHelper helper = new MimeMessageHelper( message, true, CharEncoding.UTF_8 );
+
+			helper.setSubject( subject );
+			helper.setText( content, true );
+			helper.setTo( "lethington@gmail.com" );
+
+			sender.send( message );
+
+			log.info( subject + "發送結束!" );
+
+		} catch ( MessagingException e ) {
+			throw new RuntimeException( "Failed to send: " + subject, e );
+
+		}
 	}
 }
