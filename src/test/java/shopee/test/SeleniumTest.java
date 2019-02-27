835c3035a5caea6cbe398abe3d2246631264efbc
JackSilence <jacksilence@gmail.com>
2019-02-27 11:52:21 +0800

    SeleniumTest改用headless, 並且加入log

--- src/test/java/shopee/test/SeleniumTest.java
+++ src/test/java/shopee/test/SeleniumTest.java
@@ -1,13 +1,20 @@
 package shopee.test;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.apache.commons.lang3.CharEncoding;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,17 +24,23 @@ import shopee.test.model.Search;
 @RunWith( SpringJUnit4ClassRunner.class )
 @SpringBootTest( classes = SeleniumTest.class )
 public class SeleniumTest {
+	private final Logger log = LoggerFactory.getLogger( this.getClass() );
+
 	private static final WebDriver driver;
 
 	static {
 		// System.setProperty( "webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe" );
 		WebDriverManager.chromedriver().setup();
 
-		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+
+		options.addArguments( "--headless", "--disable-gpu" ); // --disable-gpu應該是非必要設定
+
+		driver = new ChromeDriver( options );
 	}
 
 	@Test
-	public void search() throws InterruptedException {
+	public void search() throws InterruptedException, UnsupportedEncodingException {
 		driver.get( "https://shopee.tw/" );
 
 		Search search = PageFactory.initElements( driver, Search.class );
@@ -36,23 +49,36 @@ public class SeleniumTest {
 
 		search.getClose().click();
 
+		log.info( "URL before search: " + url() );
+		log.info( "Title before search: " + driver.getTitle() );
+
 		search.getInput().sendKeys( "夜燈" );
 
 		search.getButton().click();
 
 		wait( Search.XPATH_FIRST );
 
+		log.info( "URL after search: " + url() );
+		log.info( "Title after search: " + driver.getTitle() );
+
 		search.getFirst().click();
 
 		Thread.sleep( 5000 );
 
+		log.info( "URL after click first: " + url() );
+		log.info( "Title after click first: " + driver.getTitle() ); // 可能會因為頁面還沒完全載入拿到預設的, 所以放在後面
+
+		log.info( "Quit: " + driver.getClass().getSimpleName() );
+
 		driver.quit();
 	}
 
-	private void wait( String xpath ) {
-		WebDriverWait wait = new WebDriverWait( driver, 20 );
+	private String url() throws UnsupportedEncodingException {
+		return URLDecoder.decode( driver.getCurrentUrl(), CharEncoding.UTF_8 );
+	}
 
+	private void wait( String xpath ) {
 		// 也可以用css selector
-		wait.until( ExpectedConditions.presenceOfElementLocated( By.xpath( xpath ) ) );
+		new WebDriverWait( driver, 20 ).until( ExpectedConditions.presenceOfElementLocated( By.xpath( xpath ) ) );
 	}
 }
