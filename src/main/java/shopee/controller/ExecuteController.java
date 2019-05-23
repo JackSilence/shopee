89d764b470cc74dc30213a8bc6503dcf57d7b274
JackSilence <jacksilence@gmail.com>
2019-05-23 18:38:19 +0800

    更新依賴, execute get -> post

--- src/main/java/shopee/controller/ExecuteController.java
+++ src/main/java/shopee/controller/ExecuteController.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
@@ -20,7 +20,7 @@ public class ExecuteController {
 	@Autowired
 	private ApplicationContext context;
 
-	@GetMapping( value = "/execute/{name}" )
+	@PostMapping( value = "/execute/{name}" )
 	@ApiOperation( value = "手動執行排程任務", notes = "請輸入實作IService的任務Bean名稱" )
 	// @ApiImplicitParam( name = "name", value = "Bean名稱", allowableValues = "buyMiJiaTask", required = true, dataType = "String" )
 	public void execute( @ApiParam( value = "Bean名稱", allowableValues = "buyMiJiaTask,seleniumTask", required = true ) @PathVariable String name ) {
