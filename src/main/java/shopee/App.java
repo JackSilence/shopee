4556a668c4abfd1c51803fec814f7b774769c38e
JackSilence <jacksilence@gmail.com>
2019-05-23 12:24:25 +0800

    mail.debug -> false

--- src/main/java/shopee/App.java
+++ src/main/java/shopee/App.java
@@ -40,7 +40,7 @@ public class App {
 		props.put( "mail.transport.protocol", "smtp" );
 		props.put( "mail.smtp.auth", "true" );
 		props.put( "mail.smtp.starttls.enable", "true" );
-		props.put( "mail.debug", "true" );
+		props.put( "mail.debug", "false" );
 		props.put( "mail.smtp.ssl.enable", "true" );
 
 		return sender;
