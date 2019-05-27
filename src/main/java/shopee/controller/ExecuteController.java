72df197b8db8789736243d833ba326685dd44069
JackSilence <jacksilence@gmail.com>
2019-05-27 14:18:42 +0800

    更新依賴 -> d90ef8405c

--- src/main/java/shopee/controller/ExecuteController.java
+++ src/main/java/shopee/controller/ExecuteController.java
@@ -3,7 +3,6 @@ package shopee.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
@@ -11,10 +10,10 @@ import io.swagger.annotations.ApiParam;
 
 @RestController
 public class ExecuteController extends magic.controller.ExecuteController {
-	@PostMapping( value = "/execute/{name}" )
 	@ApiOperation( value = "手動執行排程任務", notes = "請輸入實作IService的任務Bean名稱" )
 	// @ApiImplicitParam( name = "name", value = "Bean名稱", allowableValues = "buyMiJiaTask", required = true, dataType = "String" )
-	public Map<String, String> execute( @ApiParam( value = "Bean名稱", allowableValues = "buyMiJiaTask,seleniumTask", required = true ) @PathVariable String name ) {
-		return super.execute( name );
+	@Override
+	public Map<String, String> execute( @ApiParam( value = "Bean名稱", allowableValues = "buyMiJiaTask,seleniumTask", required = true ) @PathVariable String name, @ApiParam( hidden = true ) String text ) {
+		return super.execute( name, text );
 	}
 }
