003eb05ee3c42851712dd7037bfa5b7144695533
JackSilence <jacksilence@gmail.com>
2019-05-28 18:38:21 +0800

    更新依賴 -> 45771f6804

--- src/main/java/shopee/controller/ExecuteController.java
+++ src/main/java/shopee/controller/ExecuteController.java
@@ -1,9 +1,9 @@
 package shopee.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.google.gson.JsonObject;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -13,7 +13,7 @@ public class ExecuteController extends magic.controller.ExecuteController {
 	@ApiOperation( value = "手動執行排程任務", notes = "請輸入實作IService的任務Bean名稱" )
 	// @ApiImplicitParam( name = "name", value = "Bean名稱", allowableValues = "buyMiJiaTask", required = true, dataType = "String" )
 	@Override
-	public JsonObject execute( @ApiParam( value = "Bean名稱", allowableValues = "buyMiJiaTask,seleniumTask", required = true ) @PathVariable String name, @ApiParam( hidden = true ) String text ) {
+	public Map<String, String> execute( @ApiParam( value = "Bean名稱", allowableValues = "buyMiJiaTask,seleniumTask", required = true ) @PathVariable String name, @ApiParam( hidden = true ) String text ) {
 		return super.execute( name, text );
 	}
 }
