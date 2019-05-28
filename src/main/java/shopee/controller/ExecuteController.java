c9ea3909ff5d71960e5b80363b4e284e1d324ae4
JackSilence <jacksilence@gmail.com>
2019-05-28 18:22:12 +0800

    更新依賴 -> 94b21fae46

--- src/main/java/shopee/controller/ExecuteController.java
+++ src/main/java/shopee/controller/ExecuteController.java
@@ -3,16 +3,17 @@ package shopee.controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.JsonObject;
+
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import net.gpedro.integrations.slack.SlackMessage;
 
 @RestController
 public class ExecuteController extends magic.controller.ExecuteController {
 	@ApiOperation( value = "手動執行排程任務", notes = "請輸入實作IService的任務Bean名稱" )
 	// @ApiImplicitParam( name = "name", value = "Bean名稱", allowableValues = "buyMiJiaTask", required = true, dataType = "String" )
 	@Override
-	public SlackMessage execute( @ApiParam( value = "Bean名稱", allowableValues = "buyMiJiaTask,seleniumTask", required = true ) @PathVariable String name, @ApiParam( hidden = true ) String text ) {
+	public JsonObject execute( @ApiParam( value = "Bean名稱", allowableValues = "buyMiJiaTask,seleniumTask", required = true ) @PathVariable String name, @ApiParam( hidden = true ) String text ) {
 		return super.execute( name, text );
 	}
 }
